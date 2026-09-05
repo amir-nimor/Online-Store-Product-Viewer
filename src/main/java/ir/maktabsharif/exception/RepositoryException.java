f405a8c9f658565435c2059938af10cce617c3e3
Amir <Soly@DESKTOP-ANSSQNA.(Soly)>
2026-09-05 20:31:42 +0330

    complite a baseRepository

---
+++ src/main/java/ir/maktabsharif/exception/RepositoryException.java
@@ -0,0 +1,7 @@
+package ir.maktabsharif.exception;
+
+public class RepositoryException extends RuntimeException {
+    public RepositoryException(String message) {
+        super(message);
+    }
+}
--- src/main/java/ir/maktabsharif/repository/BaseRepositoryImpl.java
+++ src/main/java/ir/maktabsharif/repository/BaseRepositoryImpl.java
@@ -1,36 +1,83 @@
 package ir.maktabsharif.repository;
 
+import ir.maktabsharif.exception.HibernateException;
+import ir.maktabsharif.exception.RepositoryException;
 import ir.maktabsharif.model.BaseModel;
+import ir.maktabsharif.util.HibernateUtil;
 
 import java.io.Serializable;
 import java.util.List;
 
-public class BaseRepositoryImpl <T extends BaseModel<ID>,ID extends Serializable> implements BaseRepository<T,ID>{
+public abstract class BaseRepositoryImpl<T extends BaseModel<ID>, ID extends Serializable> implements BaseRepository<T, ID> {
 
+
+    private Class<T> entityType;
+
+
+    public BaseRepositoryImpl(Class<T> entityType) {
+        this.entityType = entityType;
+    }
 
 
     @Override
     public ID save(T t) {
-        return null;
+        try {
+            return HibernateUtil.inTxReturn(em -> {
+                em.persist(t);
+                return t.getId();
+            });
+        } catch (HibernateException e) {
+            throw new RepositoryException("save operation is failed => " + e.getMessage());
+        }
     }
 
     @Override
     public T update(T t) {
-        return null;
+        try {
+            return HibernateUtil.inTxReturn(em -> {
+                T entity = em.find(entityType, t.getId());
+                updateEntity(t, entity);
+                return entity;
+            });
+        } catch (HibernateException e) {
+            throw new RepositoryException("update operation is failed => " + e.getMessage());
+        }
     }
 
     @Override
     public ID delete(ID id) {
-        return null;
+        try {
+            return HibernateUtil.inTxReturn(em -> {
+                em.remove(em.find(entityType,id));
+                return id;
+            });
+        }catch (HibernateException e){
+            throw new RepositoryException("delete operation is failed => "+e.getMessage());
+        }
     }
 
     @Override
     public T read(ID id) {
-        return null;
+        try {
+            return HibernateUtil.read(em -> {
+                return em.find(entityType,id);
+            });
+        }catch (HibernateException e){
+            throw new RepositoryException("read operation is failed => "+e.getMessage());
+        }
     }
 
     @Override
     public List<T> findAll() {
-        return List.of();
+        try {
+            return HibernateUtil.read(em -> {
+                return em.createQuery("from "+entityType)
+                        .getResultList();
+            });
+        }catch (HibernateException e){
+            throw new RepositoryException("find All operation is failed => "+e.getMessage());
+        }
     }
+
+    protected abstract void updateEntity(T newEntity, T dbEntity);
 }
